53e57037fc5812b81a49ee23bae3d99ca0bcf2f7
Muhammet Matiyev <79673059+matiyev@users.noreply.github.com>
2024-01-08 19:40:40 +0200

    MoodSnap added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,61 +1,15 @@
 import React from "react";
 import "./portfolio.css";
-// import IMG1 from "../../assets/brights-lite.png";
-// import IMG2 from "../../assets/blog.png";
-// import IMG3 from "../../assets/calculator.png";
-// import IMG4 from "../../assets/brochure.png";
-// import IMG5 from "../../assets/Portfolio3.png";
-// import IMG6 from "../../assets/Portfolio4.png";
+import IMG1 from "../../assets/MoodSnap.jpeg";
 
 const data = [
   {
     id: 1,
-    // image: IMG1,
-    title: "Real Time Face Detection",
-    github: "https://github.com/matiyev",
-    demo: <a href="faceDetection/index.html"></a>,
-  },
-  // {
-  //   id: 2,
-  //   image: IMG2,
-  //   title: "NextJS | TailwindCSS | GraphQL Blog",
-  //   github: "https://github.com/Ade-mir/nextjs-blog",
-  //   demo: "https://musical-puffpuff-ff26f2.netlify.app/",
-  //   brief: "https://github.com/Ade-mir/nextjs-blog/blob/main/README.md",
-  // },
-  // {
-  //   id: 3,
-  //   image: IMG3,
-  //   title: "JavaScript | HTML | CSS Calculator",
-  //   github: "https://github.com/Ade-mir/javascript-calculator",
-  //   demo: "https://fabulous-otter-35c137.netlify.app/",
-  //   brief:
-  //     "https://github.com/Ade-mir/javascript-calculator/blob/main/README.md",
-  // },
-  // {
-  //   id: 4,
-  //   image: IMG4,
-  //   title: "React Brochure Site",
-  //   github: "https://github.com/Ade-mir/brochure-site",
-  //   demo: "https://deluxe-gumption-ef08f4.netlify.app/",
-  //   brief: "https://github.com/Ade-mir/brochure-site/blob/main/README.md",
-  // },
-  // {
-  //   id: 5,
-  //   image: IMG5,
-  //   title: "JavaScript | HTML | CSS Portfolio",
-  //   github: "https://github.com/Ade-mir/html-css-js-portfolio-tutorial-2",
-  //   demo: "https://thriving-dragon-a3fcd9.netlify.app/",
-  //   brief: "https://youtu.be/ldwlOzRvYOU",
-  // },
-  // {
-  //   id: 6,
-  //   image: IMG6,
-  //   title: "JavaScript | HTML | CSS Animation Head",
-  //   github: "https://github.com/Ade-mir/animated-head-portfolio",
-  //   demo: "https://animated-head-portfolio.netlify.app/",
-  //   brief: "https://youtu.be/pcFF_IIpMbc",
-  // },
+    image: IMG1,
+    title: "MoodSnap | Mood Detection",
+    github: "https://github.com/matiyev/MoodSnap",
+    demo: "https://moodsnap.netlify.app/",
+  }
 ];
 
 const Portfolio = () => {
@@ -79,11 +33,6 @@ const Portfolio = () => {
                   Live Demo
                 </a>
               </div>
-              {/* <div className="portfolio__item-cta">
-                <a href={demo} className="btn btn-primary">
-                  Live Demo
-                </a>
-              </div> */}
             </article>
           );
         })}
