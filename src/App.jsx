9ab432df76f94b09043a84d5b65c1ee390e8d558
Muhammet Matiyev <79673059+matiyev@users.noreply.github.com>
2024-01-15 21:38:20 +0200

    portfolio section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./components/nav/Nav";
 import Experience from "./components/experience/Experience";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-// import Portfolio from "./components/portfolio/Portfolio";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <Header />
       <Nav />
       <Experience />
-      {/* <Portfolio /> */}
+      <Portfolio />
       <Contact />
       <Footer />
     </>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/MoodSnap.jpeg";
+import IMG2 from "../../assets/soc.jpg";
+import IMG3 from "../../assets/homelab.jpeg";
+import IMG4 from "../../assets/security-onion.jpeg";
+import IMG5 from "../../assets/nmap.jpg";
+import IMG6 from "../../assets/wireshark.jpg";
 
 const data = [
   {
@@ -9,7 +14,37 @@ const data = [
     title: "MoodSnap | Mood Detection",
     github: "https://github.com/matiyev/MoodSnap",
     demo: "https://moodsnap.netlify.app/",
-  }
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Fast-track to SOC Excellence",
+    github: "https://github.com/matiyev/soc-lab"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Monitoring & Detection Homelab",
+    github: "https://github.com/matiyev/monitoring-detection-lab"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "PCAP investigation | Security Onion",
+    github: "https://github.com/matiyev/pcap-investigation"
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Network Troubleshooting | Nmap & hping3",
+    github: "https://github.com/matiyev/network-troubleshooting"
+  },
+  {
+    id: 5,
+    image: IMG6,
+    title: "Intercepting & Analyzing Network Traffic | Wireshark & tcpdump",
+    github: "https://github.com/matiyev/network-traffic-analysis"
+  },
 ];
 
 const Portfolio = () => {
@@ -18,7 +53,7 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo, brief }) => {
+        {data.map(({ id, image, title, github, demo }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
@@ -29,9 +64,11 @@ const Portfolio = () => {
                 <a href={github} className="btn">
                   Github
                 </a>
-                <a href={demo} className="btn btn-primary">
-                  Live Demo
-                </a>
+                {id === 1 && (
+                  <a href={demo} className="btn btn-primary">
+                    Live Demo
+                  </a>
+                )}
               </div>
             </article>
           );
